a5cd79392ba2c305dd4cf3f44b37390197cecdd0
EriaWist <49594697+EriaWist@users.noreply.github.com>
2023-09-26 03:22:20 +0800

    設定fetchData與viewModel告知

---
+++ goonsdesign-search-repositories/Model/Repositories.swift
@@ -0,0 +1,26 @@
+//
+//  Repositories.swift
+//  goonsdesign-search-repositories
+//
+//  Created by 黃子騰 on 2023/9/26.
+//
+
+import Foundation
+
+struct Repositories:Codable{
+    var items:[RepositoriesItem]
+    var total_count:Int
+}
+struct RepositoriesItem:Codable{
+    var full_name:String
+    var description:String?
+    var language:String?
+    var owner:RepositoriesOwner
+    var stargazers_count:Int
+    var watchers_count:Int
+    var forks_count:Int
+    var open_issues_count:Int
+}
+struct RepositoriesOwner:Codable{
+    var avatar_url:String
+}
--- goonsdesign-search-repositories/ViewModel/SearchViewModel.swift
+++ goonsdesign-search-repositories/ViewModel/SearchViewModel.swift
@@ -6,7 +6,45 @@
 //
 
 import Foundation
+import Combine
 
 class SearchViewModel{
     weak var delegate: ViewModelDelegate?
+    var cancellBag = Set<AnyCancellable>()
+    private var _repositoriesItem:[RepositoriesItem] = []
+    {
+        didSet{
+            delegate?.update()
+        }
+    }
+    var repositoriesItem:[RepositoriesItem]{
+        get{
+            _repositoriesItem
+        }
+    }
+    func fetchData(query:String?) {
+        _repositoriesItem = []
+        if let query = query{
+            repositoriesPublisher(query: query)
+                .sink(receiveCompletion: {error in
+                    print(error)
+                }, receiveValue: { [weak self] repositories in
+                    self?._repositoriesItem = repositories.items
+                })
+                .store(in: &cancellBag)
+        }
+    }
+    private func repositoriesPublisher(query:String) -> AnyPublisher<Repositories,Error> {
+        return URLSession.shared.dataTaskPublisher(for: URL(string: "https://api.github.com/search/repositories?q=\(query)")!)
+            .tryMap() { element -> Data in
+                guard let httpResponse = element.response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else {
+                    print("2--")
+                    throw URLError(.badServerResponse)
+                }
+                return element.data
+            }
+            .decode(type: Repositories.self, decoder: JSONDecoder())//解碼
+            .eraseToAnyPublisher()//轉換成AnyPublisher
+    }
 }
