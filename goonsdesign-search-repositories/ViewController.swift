87a55939126e43d8c787be60352700ff332b7e24
EriaWist <49594697+EriaWist@users.noreply.github.com>
2023-09-26 00:43:02 +0800

    建立MVVM架接相關

--- goonsdesign-search-repositories/ViewController.swift
+++ goonsdesign-search-repositories/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var viewModel = SearchViewModel()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewModel.delegate = self
     }
 
 
@@ -23,7 +25,15 @@ extension ViewController:UITableViewDataSource,UITableViewDelegate{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.imageView?.image = UIImage(systemName: "doc.badge.ellipsis")
+        return cell
     }
     
+}
+
+extension ViewController:ViewModelDelegate{
+    func update() {
+        tableView.reloadData()
+    }
 }
---
+++ goonsdesign-search-repositories/ViewModel/SearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewModel.swift
+//  goonsdesign-search-repositories
+//
+//  Created by 黃子騰 on 2023/9/25.
+//
+
+import Foundation
+
+class SearchViewModel{
+    weak var delegate: ViewModelDelegate?
+}
