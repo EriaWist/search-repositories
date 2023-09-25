002ea5bf2f7f6b038af3854abd6c0ccef8fbc671
EriaWist <49594697+EriaWist@users.noreply.github.com>
2023-09-25 21:06:38 +0800

    Initial Commit

---
+++ goonsdesign-search-repositories/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  goonsdesign-search-repositories
+//
+//  Created by 黃子騰 on 2023/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+}
